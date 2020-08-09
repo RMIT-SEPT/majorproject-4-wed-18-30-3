1d50bc94948fc7912d9f15f2cf2673c503d02e0c
zhihang zhang s3737542 <s3737542@student.rmit.edu.au>
2020-08-10 02:40:48 +1000

    creating maven project with spring boot framwork

---
+++ BackEnd/MajorProject/src/test/java/labwed18303/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package labwed18303.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
