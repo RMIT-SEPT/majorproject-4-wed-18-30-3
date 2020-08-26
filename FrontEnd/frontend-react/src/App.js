fadbd5b53108ee794e84a0d72c870af587f8ecc6
s-brez <sam@sdbgroup.io>
2020-08-26 15:35:31 +1000

    add react-router to project

--- FrontEnd/frontend-react/src/App.js
+++ FrontEnd/frontend-react/src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Header></Header>
-      <Dashboard></Dashboard>
+      <Header/>
+      <Route exact path="/dashboard" component={Dashboard}/>
+      
     </div>
+    </Router>
   );
 }
 
--- FrontEnd/frontend-react/src/components/Layout/Header.js
+++ FrontEnd/frontend-react/src/components/Layout/Header.js
@@ -7,20 +7,17 @@ class Header extends Component {
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <div className="container">
                 
-                <a className="navbar-brand" href="Dashboard.html">
-                    User manangement
+                <a className="navbar-brand" href="dashboard.html">
+                    Home
                 </a>
-
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav" aria-controls="mobile-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
                     <span className="navbar-toggler-icon"/>
                 </button>
 
                 <div className="collapse navbar-collapse" id="mobile-nav">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="/Dashboard">
-                                Home
-                            </a>
+
                         </li>
                     </ul>
 
