1bed045af1db8122e4be77051db48a6a742ebc96
s-brez <sam@sdbgroup.io>
2020-08-26 12:44:01 +1000

    change default app index page to display group details

--- FrontEnd/frontend-react/src/App.js
+++ FrontEnd/frontend-react/src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Booking Scheduling App - Group LAB-WED-6.30-Ujj-Group3</h2>
     </div>
   );
 }
