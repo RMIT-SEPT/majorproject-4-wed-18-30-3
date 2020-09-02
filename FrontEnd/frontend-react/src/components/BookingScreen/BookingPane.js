62152e0e17f5d7454c985c173f4e58aa0401fa9d
s-brez <sam@sdbgroup.io>
2020-09-02 18:00:47 +1000

    state management and POST working for bookings screen

--- FrontEnd/frontend-react/src/components/BookingScreen/BookingPane.js
+++ FrontEnd/frontend-react/src/components/BookingScreen/BookingPane.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import DateTimePicker from 'react-datetime-picker'
-import AddBookingButton from './AddBookingButton';
+import axios from "axios";
 import CancelButton from './CancelButton';
 
     function roundTo(date) {
@@ -9,33 +9,61 @@ import CancelButton from './CancelButton';
         // Assumed 15 
 
         var mins = 1000 * 60 * 15;
+
         return new Date(Math.round(date.getTime() / mins) * mins)
     }
 
-    function createBooking() {
-        var timeslot = this.state.date;
+    function createBooking(state) {
 
-        var i = document.getElementById("service");
-        var service = i.options[i.selectedIndex].value;
-        
-        var i = document.getElementById("duration");
-        var duration = i.options[i.selectedIndex].value;
-        
-        var i = document.getElementById("worker");
-        var worker = i.options[i.selectedIndex].value;
+        var timeslot = state.date.getTime();
+        var service = state.service;
+        var duration = state.service;
+        var worker = state.worker;
+        var customer = state.customer;
+        var group = state.group;
 
-        var customer = null;
-        var group = null;
+        console.log("test");
 
-        alert(timeslot + service + duration + worker + customer + group)
+        axios.post('https://ae5b398a-4768-4d8f-b24a-0f5aa15a09a0.mock.pstmn.io/bookings', {
+            timeslot: timeslot,
+            service: service,
+            duration: duration,
+            worker: worker,
+            customer: customer,
+            group: group
+        })
+        .then(res => {
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log(res)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    
     }
     
 class BookingPane extends Component {
 
-    state = {date: roundTo(new Date()),}
+
+
+    constructor() {
+        super()
+        this.state = {
+            date: new Date(),
+            duration: "",
+            service: "",
+            worker: "",
+            customer: "",
+            group: "",
+            
+        }
+    }
 
     onChange = date => this.setState({ date: roundTo(date) })
-
+    onChange = duration => this.setState({ duration })
+    onChange = service => this.setState({ service })
+    onChange = worker => this.setState({ worker })
+    
     render() {
         return (
             <div className="booking_screen_bookingpane" id="booking_screen_bookingpane">
@@ -46,39 +74,31 @@ class BookingPane extends Component {
                     <p>Select desired start time and date (please note 15 minute minimum duration):</p>
                     <DateTimePicker onChange={this.onChange} value={roundTo(this.state.date)}/>
                     <br/> <br/>    
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="duration">Select job duration:</label>
-                        <select class="form-control" id="duration">
+                        <select className="form-control" value={this.state.duration} onChange={this.onChange}>
                             <option value="15">15 min</option>
                             <option value="30">30 min</option>
                             <option value="45">45 min</option>
-                            <option value="60">1 hour</option>
+                            <option value="60" >1 hour</option>
                             <option value="75">1 hr 15 min</option>
                             <option value="90">1 hr 30 min</option>
                             <option value="105">1 hr 45 min</option>
                             <option value="120">2 hours</option>
-                            <option value="135">2 hr 15 min</option>
-                            <option value="150">2 hr 30 min</option>
-                            <option value="165">2 hr 45 min</option>
-                            <option value="180">3 hours</option>
-                            <option value="195">3 hr 15 min</option>
-                            <option value="210">3 hr 30 min</option>
-                            <option value="225">3 hr 45 min</option>
-                            <option value="240">4 hours</option>
                         </select>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                     <label for="service">Select a service:</label>
-                    <select class="form-control" id="service">
-                      <option value="1">Service 1</option>
+                    <select className="form-control" value={this.state.service} onChange={this.onChange}>
+                      <option value="1" >Service 1</option>
                       <option value="2">Service 2</option>
                       <option value="3">Service 3</option>
                     </select>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                     <label for="worker">Select an available worker:</label>
-                    <select class="form-control" id="worker">
-                      <option value="1">Worker 1</option>
+                    <select className="form-control" value={this.state.worker} onChange={this.onChange}>
+                      <option value="1" >Worker 1</option>
                       <option value="2">Worker 2</option>
                       <option value="3">Worker 3</option>
                     </select>
@@ -86,7 +106,7 @@ class BookingPane extends Component {
 
                     <div className="row">
                         <div className="col-sm">
-                           <button className="btn btn-sm btn-dark" id="navButton" onlick={createBooking}>
+                           <button onSubmit={createBooking(this.state)} className="btn btn-sm btn-dark" id="navButton">
                             Create new booking
                            </button>
                         </div>
