e0cb0325481231a0218a33988a90f532a4f5c031
s3719826 <41975799+s3719826@users.noreply.github.com>
2020-09-09 19:44:08 +1000

    start of login ui

---
+++ FrontEnd/login_ui/Login Ui.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Login_Ui extends React.Component {
+    render(){
+        return(
+            <div className = "Login_Ui">
+                <div className = "Heading">
+                    <h1 header> Log in</h1>
+                    <br></br><br></br><br></br><br></br>
+                </div>
+                <div className = "UserName">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="uname"></input><br></br><br></br>
+                </div>
+                <div className = "PasswordName">
+                <label for="password">Password:</label>
+                <input type="text" id="password" name="pass"></input><br></br><br></br>
+                </div>
+            </div>
+        )
+    }
+}
