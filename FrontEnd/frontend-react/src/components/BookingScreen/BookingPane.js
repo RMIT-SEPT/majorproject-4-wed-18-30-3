a4f4b97b8d9a084556088ef9c07e1ec9c9222f30
s-brez <sam@sdbgroup.io>
2020-09-09 21:41:34 +1000

    input state save working

--- FrontEnd/frontend-react/src/components/BookingScreen/BookingPane.js
+++ FrontEnd/frontend-react/src/components/BookingScreen/BookingPane.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 import DateTimePicker from 'react-datetime-picker'
 import axios from "axios";
 import CancelButton from './CancelButton';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 
+    // Round a date object to its nearest 15min increment
     function roundTo(date) {
         
         // Change final number to match min booking duration
         // Assumed 15 
+        if (date != null) {
+            var mins = 1000 * 60 * 15;
+            return new Date(Math.round(date.getTime() / mins) * mins)
+        } else 
+            return date
 
-        var mins = 1000 * 60 * 15;
-        return new Date(Math.round(date.getTime() / mins) * mins)
     }
 
     var config = {
@@ -48,35 +54,49 @@ class BookingPane extends Component {
         super()
         this.state = {
             date: roundTo(new Date()),
-            duration: "",
-            service: "",
-            worker: "",
-            customer: "",
+            duration: null,
+            service: null,
+            worker: null,
+            customer: null,
         }
 
-        this.onChange = this.onChange.bind(this);
+        // this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
-    onChange(e){
-        if (e.name === "date")
-            this.setState({[e.name]: roundTo(e.value)});
-        else
-            this.setState({[e.name]: e.value});
-    }
+    onDateChange = date => this.setState({ date })
+    onDurationChange = duration => this.setState({ duration })
+    onServiceChange = service => this.setState({ service })
+    onWorkerChange = worker => this.setState({ worker })
     
     onSubmit(e){
         e.preventDefault();
 
+        if (this.state.date == null) {
+            alert("Please select a start time and date.")
+        }
+        if (this.state.duration == null) {
+            alert("Please select a duration.")
+        }
+        if (this.state.service == null) {
+            alert("Please select a service.")
+        }
+        if (this.state.worker == null) {
+            alert("Please select a worker.")
+        }            
+
         // Need to match this with Cams backend format
         var timeslot = roundTo(this.state.date)
 
+        // Get this from local React state once login is done
+        var customer = null;
+
         const newBooking = {
             timeslot: timeslot,
-            duration: this.state.duration,
-            service: this.state.service,
-            worker: this.state.worker,
-            customer: this.state.customer
+            duration: this.state.duration.value,
+            service: this.state.service.value,
+            worker: this.state.worker.value,
+            customer: customer
         }
 
         // Send the POST request
@@ -84,6 +104,30 @@ class BookingPane extends Component {
     }
 
     render() {
+        const animatedComponents = makeAnimated();
+        var durationOptions = [
+            { value: 15, label: '15 minutes' },
+            { value: 30, label: '30 minutes' },
+            { value: 45, label: '45 minutes' },
+            { value: 60, label: '1 hour' },
+            { value: 75, label: '1 hr 15 mins' },
+            { value: 90, label: '1 hr 30 mins' },
+            { value: 105, label: '1 hr 45 mins' },
+            { value: 120, label: '2 hours' },
+        ]
+
+        var serviceOptions = [
+            { value: 'mowing', label: 'Mowing' },
+            { value: 'hedging', label: 'Hedging' },
+            { value: 'pool_cleaning', label: 'Pool Cleaning' },
+        ]
+
+        var workerOptions = [
+            { value: 'jim', label: 'Jim' },
+            { value: 'tim', label: 'Tim' },
+            { value: 'rob', label: 'Rob' },
+        ]
+        
         return (
             <div className="booking_screen_bookingpane" id="booking_screen_bookingpane">
                 <br/>    
@@ -91,38 +135,23 @@ class BookingPane extends Component {
                 <br/>   
                 <form onSubmit={this.onSubmit}>
                     <p>Select desired start time and date. Actual start time rounded to the closest 15 minutes:</p>
-                    <DateTimePicker onChange={this.onChange} value={this.state.date}/>
+                    <DateTimePicker name="date" value={this.state.date} onChange={this.onDateChange}/>
                     <br/> <br/>    
                     <div className="form-group">
                         <label htmlFor="duration">Select job duration:</label>
-                        <select className="form-control" value={this.state.duration} onChange={this.onChange} name="duration">
-                            <option value="15">15 min</option>
-                            <option value="30">30 min</option>
-                            <option value="45">45 min</option>
-                            <option value="60" >1 hour</option>
-                            <option value="75">1 hr 15 min</option>
-                            <option value="90">1 hr 30 min</option>
-                            <option value="105">1 hr 45 min</option>
-                            <option value="120">2 hours</option>
-                        </select>
+                        <Select name={"duration"} value={this.state.duration} options={durationOptions} 
+                            onChange={this.onDurationChange} components={animatedComponents}/>
                     </div>
                     <div className="form-group">
-                    <label htmlFor="service">Select a service:</label>
-                    <select className="form-control" value={this.state.service} onChange={this.onChange}>
-                      <option value="1" >Service 1</option>
-                      <option value="2">Service 2</option>
-                      <option value="3">Service 3</option>
-                    </select>
+                        <label htmlFor="service">Select a service:</label>
+                        <Select name={"service"} value={this.state.value} options={serviceOptions}
+                            onChange={this.onServiceChange} components={animatedComponents}/>
                     </div>
                     <div className="form-group">
                     <label htmlFor="worker">Select an available worker:</label>
-                    <select className="form-control" value={this.state.worker} onChange={this.onChange}>
-                      <option value="1" >Worker 1</option>
-                      <option value="2">Worker 2</option>
-                      <option value="3">Worker 3</option>
-                    </select>
+                    <Select name={"worker"} value={this.state.value} options={workerOptions}
+                        onChange={this.onWorkerChange} components={animatedComponents}/>
                     </div>
-
                     <div className="row">
                         <div className="col-sm">
                            <input type="submit" className="btn btn-sm btn-dark" id="navButton"/>
