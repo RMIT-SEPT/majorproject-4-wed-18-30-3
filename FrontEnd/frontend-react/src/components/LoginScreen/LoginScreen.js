aa67457499b025ad1b199e8d4363310b4fccee5a
s3719826 <41975799+s3719826@users.noreply.github.com>
2020-09-14 16:19:13 +1000

    register,login, need backend

--- FrontEnd/frontend-react/src/components/LoginScreen/LoginScreen.js
+++ FrontEnd/frontend-react/src/components/LoginScreen/LoginScreen.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
+import {Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Dashboard from '../Dashboard/Dashboard';
+
 
 class LoginScreen extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Todo: Login Screen</h1>
+    constructor() {
+        super();
+        this.state = {
+          userName: null,
+          password: null,
+        };
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        if(username == null){
+            alert("Please enter a username!")
+            return
+        }
+        if(password == null){
+            alert("Please enter a password!")
+            return
+        }
+        this.setState({ userName: document.getElementById("username").value})
+        this.setState({ password: document.getElementById("password").value})
+        // confirmation of user in here
+        //if its confirmed then this happens
+        if(true){
+            //set the dashboards user as the login user
+            <Link to="/dashboard"></Link>
+        }
+        //user doesnt exist / password incorrect
+        else{
+            alert("Invalid password or username, please try again!")
+        }
+      }
+    render(){
+        return(
+            <div className = "Login_Ui">
+                <div className = "Heading">
+                    <h1 header> Log in</h1>
+                    <br></br><br></br><br></br><br></br>
+                </div>
+                <div className = "UserName">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="uname" required></input><br></br><br></br>
+                </div>
+                <div className = "Password">
+                <label for="password">Password:</label>
+                <input type="text" id="password" name="pass" required></input><br></br><br></br>
+                </div>
+                <input type="submit" className="login_Submit" id="navButton"/>
             </div>
+
         )
     }
 }
--- FrontEnd/frontend-react/src/components/RegisterScreen/RegisterScreen.js
+++ FrontEnd/frontend-react/src/components/RegisterScreen/RegisterScreen.js
@@ -1,13 +1,118 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
-
-
+import makeAnimated from 'react-select/animated';
+import Select from 'react-select';
 class RegisterScreen extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Todo: Register Screen</h1>
+    constructor() {
+        super();
+        this.state = {
+          userName: null,
+          password: null,
+          phone: null,
+          address: null,
+          firstName: null,
+          lastName: null,
+          userType:null,
+        };
+        onServiceChange = userType => this.setState({ userType })
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        if(username == null){
+            alert("Please enter a username!")
+            return
+        }
+        if(password == null){
+            alert("Please enter a password!")
+            return
+        }
+        if(phone == null){
+            alert("Please enter a phone number!")
+            return
+        }
+        let phoneNumberLength = this.state.phone.length
+        if(phoneNumberLength >10){
+            alert("invalid phone number!")
+            return
+        }
+        if(phoneNumberLength >10){
+            alert("invalid phone number!")
+            return
+        }
+        if(address == null){
+            alert("Please enter your address!")
+            return
+        }
+        if(firstName == null){
+            alert("Please enter a first name!")
+            return
+        }
+        if(lastName == null){
+            alert("Please enter a last name!")
+            return
+        }
+        this.setState({ userName: document.getElementById("username").value})
+        this.setState({ password: document.getElementById("password").value})
+        this.setState({ phone: document.getElementById("phone").value})
+        this.setState({ address: document.getElementById("address").value})
+        this.setState({ firstName: document.getElementById("firstName").value})
+        this.setState({ lastName: document.getElementById("lastName").value})
+        // confirmation that username doesnt exist
+        //if its confirmed then this happens
+        if(true){
+            //post of user in here
+            <Link to="/loginscreen"></Link>
+        }
+        //username is taken
+        else{
+            alert("username is taken, please try again!")
+        }
+      }
+    render(){
+        const animatedComponents = makeAnimated();
+        const userTypes = [
+            { value: 'Customer', label: 'Customer' },
+            { value: 'Worker', label: 'Worker' },
+            { value: 'Admin', label: 'Admin' },
+        ]
+        return(
+            <div className = "Register_Ui">
+                <div className = "Heading">
+                    <h1 header> Sign Up</h1>
+                    <br></br><br></br><br></br><br></br>
+                </div>
+                <div className = "UserName">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="uname" required></input><br></br><br></br>
+                </div>
+                <div className = "Password">
+                <label for="password">Password:</label>
+                <input type="text" id="password" name="pass" required></input><br></br><br></br>
+                </div>
+                <div className = "Phone">
+                <label for="phone">Password:</label>
+                <input type="text" id="phone" name="phone" required></input><br></br><br></br>
+                </div>
+                <div className = "Address">
+                <label for="address">Password:</label>
+                <input type="text" id="address" name="address" required></input><br></br><br></br>
+                </div>
+                <div className = "Firstname">
+                <label for="firstName">Password:</label>
+                <input type="text" id="firstName" name="fName" required></input><br></br><br></br>
+                </div>
+                <div className = "Lastname">
+                <label for="lastName">Password:</label>
+                <input type="text" id="lastName" name="lName" required></input><br></br><br></br>
+                </div>
+                <div className="from-user">
+                        <label for="user">Select job duration:</label>
+                        <Select name="user" id="user" value={this.state.userType} options={userTypes} 
+                            onChange={this.onDurationChange} components={animatedComponents}/>
+                    </div>
+                <input type="submit" className="login_Submit" id="navButton"/>
             </div>
+
         )
     }
 }
