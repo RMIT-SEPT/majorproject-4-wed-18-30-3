c4c42e6e8fbd1ca09add26ed7b09c8191385ff08
s3719826 <41975799+s3719826@users.noreply.github.com>
2020-09-15 22:44:26 +1000

    Fixed some submission errors, still need backend

--- FrontEnd/frontend-react/src/components/LoginScreen/LoginScreen.js
+++ FrontEnd/frontend-react/src/components/LoginScreen/LoginScreen.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
-//import {Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
-//import Dashboard from '../Dashboard/Dashboard';
-
+import Dashboard from '../Dashboard/Dashboard';
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
+//import Header from '../Layout/Header';
+//import Footer from '../Layout/Footer';
+//import NavPane from '../Layout/NavPane';
 
 class LoginScreen extends Component {
     constructor() {
         super();
         this.state = {
-          userName: null,
-          password: null
+            userName: null,
+            password: null,
+            hasSuccess: false,
+            hasFail: false,
         }
+        this.onSubmit = this.onSubmit.bind(this);
     }
-    onSubmit(e) {
-        e.preventDefault();
+    setUsername = () => this.setState({ userName: document.getElementById("username").value})
+    setPassword = () => this.setState({ password: document.getElementById("password").value})
 
+    async onSubmit(e) {
+        e.preventDefault();
+        await this.setPassword()
+        await this.setUsername()
         if(this.state.userName == null){
             alert("Please enter a username!")
             return
@@ -23,39 +32,53 @@ class LoginScreen extends Component {
             alert("Please enter a password!")
             return
         }
-        this.setState({ userName: document.getElementById("username").value})
-        this.setState({ password: document.getElementById("password").value})
         // confirmation of user in here
         //if its confirmed then this happens
-        if(true){
-            //set the dashboards user as the login user
-            //<Link to="/dashboard"></Link>
+        // const success = await (method)().then()
+        const success = true;
+        if (success) {
+            this.setState({hasSuccess: true})
+            this.setState({hasFail: false})
+        } else {
+            this.setState({hasSuccess: false})
+            this.setState({hasFail: true})
         }
-        //user doesnt exist / password incorrect
-        else{
-            alert("Invalid password or username, please try again!")
-        }
-      }
-    render(){
+    }
+    render() {
+    if (!this.state.hasSuccess && !this.state.hasFail) {
         return(
             <div className = "Login_Ui">
                 <div className = "Heading">
                     <h1> Log in</h1>
                     <br></br><br></br><br></br><br></br>
                 </div>
-                <div className = "UserName">
-                <label htmlFor={"username"}>Username:</label>
-                <input type="text" id="username" name="uname" required></input><br></br><br></br>
-                </div>
-                <div className = "Password">
-                <label htmlFor={"password"}>Password:</label>
-                <input type="text" id="password" name="pass" required></input><br></br><br></br>
-                </div>
-                <input type="submit" className="login_Submit" id="navButton"/>
+                <form onSubmit={this.onSubmit}>
+                    <div className = "UserName">
+                        <label htmlFor={"username"}>Username:</label>
+                        <input type="text" id="username" name="uname" required></input><br></br><br></br>
+                    </div>
+                    <div className = "Password">
+                        <label htmlFor={"password"}>Password:</label>
+                        <input type="password" id="password" name="pass" required></input><br></br><br></br>
+                    </div>
+                    <input type="submit" className="login_Submit" id="navButton"/>
+                </form>
             </div>
 
+            )
+        }
+    if(this.state.hasSuccess) {
+        return(
+        <Router>
+            <Route exact path="/">
+                <Redirect to="/dashboard" component = {Dashboard}/>
+            </Route>
+        </Router>
         )
     }
+    else if(this.state.hasFail){
+        alert("Password or Username incorrect!")
+    }
+    }
 }
-
 export default LoginScreen;
--- FrontEnd/frontend-react/src/components/RegisterScreen/RegisterScreen.js
+++ FrontEnd/frontend-react/src/components/RegisterScreen/RegisterScreen.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import makeAnimated from 'react-select/animated';
 import Select from 'react-select';
-//import {Link} from "react-router-dom";
+import LoginScreen from '../LoginScreen/LoginScreen'
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 class RegisterScreen extends Component {
     constructor() {
         super();
@@ -15,20 +16,26 @@ class RegisterScreen extends Component {
           lastName: null,
           userType:null,
         };
+        this.onSubmit = this.onSubmit.bind(this);
     }
+    setUsername = () => this.setState({ userName: document.getElementById("username").value})
+    setPassword = () => this.setState({ password: document.getElementById("password").value})
+    setPhone = () => this.setState({ phone: document.getElementById("phone").value})
+    setAddress = () => this.setState({ address: document.getElementById("address").value})
+    setFirstName = () => this.setState({ firstName: document.getElementById("firstName").value})
+    setLastName = () => this.setState({ lastName: document.getElementById("lastName").value})
     onTypeChange(Type) {
         this.setState({ userName: Type})
     }
     onTypeChange = userType => this.setState({ userType })
-    onSubmit(e) {
+    async onSubmit(e) {
         e.preventDefault();
-        this.setState({ userName: document.getElementById("username").value})
-        this.setState({ password: document.getElementById("password").value})
-        this.setState({ phone: document.getElementById("phone").value})
-        this.setState({ address: document.getElementById("address").value})
-        this.setState({ firstName: document.getElementById("firstName").value})
-        this.setState({ lastName: document.getElementById("lastName").value})
-        let phoneNumberLength = this.state.phone.length
+        await this.setPassword()
+        await this.setUsername()
+        await this.setPhone()
+        await this.setAddress()
+        await this.setFirstName()
+        await this.setLastName()
         if(this.state.userName == null){
             alert("Please enter a username!")
             return
@@ -38,14 +45,15 @@ class RegisterScreen extends Component {
             return
         }
         if(this.state.phone == null){
-            alert("Please enter a phone number!")
+            alert("Please enter a valid phone number!")
             return
         }
+        let phoneNumberLength = this.state.phone.length
         if(phoneNumberLength >10){
             alert("invalid phone number!")
             return
         }
-        if(phoneNumberLength >10){
+        if(phoneNumberLength <10){
             alert("invalid phone number!")
             return
         }
@@ -61,16 +69,23 @@ class RegisterScreen extends Component {
             alert("Please enter a last name!")
             return
         }
+        if(this.state.userType == null){
+            alert("Please select what type of user you are!")
+            return
+        }
         // confirmation that username doesnt exist
         //if its confirmed then this happens
-        if(true){
-            //post of user in here
-            //<Link to="/loginscreen"></Link>
+        // const success = await (method)().then()
+        const success = true;
+        if (success) {
+            this.setState({hasSuccess: true})
+            this.setState({hasFail: false})
+        } else {
+            this.setState({hasSuccess: false})
+            this.setState({hasFail: true})
         }
         //username is taken
-        else{
-            alert("username is taken, please try again!")
-        }
+
       }
     render(){
         const animatedComponents = makeAnimated();
@@ -79,45 +94,60 @@ class RegisterScreen extends Component {
             { value: 'Worker', label: 'Worker' },
             { value: 'Admin', label: 'Admin' },
         ]
-        return(
-            <div className = "Register_Ui">
-                <div className = "Heading">
-                    <h1> Sign Up</h1>
-                    <br></br><br></br><br></br><br></br>
-                </div>
-                <div className = "UserName">
-                <label htmlFor={"username"}>Username:</label>
-                <input type="text" id="username" name="uname" required></input><br></br><br></br>
-                </div>
-                <div className = "Password">
-                <label htmlFor={"password"}>Password:</label>
-                <input type="text" id="password" name="pass" required></input><br></br><br></br>
-                </div>
-                <div className = "Phone">
-                <label htmlFor={"phone"}>Password:</label>
-                <input type="text" id="phone" name="phone" required></input><br></br><br></br>
-                </div>
-                <div className = "Address">
-                <label htmlFor={"address"}>Password:</label>
-                <input type="text" id="address" name="address" required></input><br></br><br></br>
-                </div>
-                <div className = "Firstname">
-                <label htmlFor={"firstName"}>Password:</label>
-                <input type="text" id="firstName" name="fName" required></input><br></br><br></br>
-                </div>
-                <div className = "Lastname">
-                <label htmlFor={"lastName"}>Password:</label>
-                <input type="text" id="lastName" name="lName" required></input><br></br><br></br>
-                </div>
-                <div className="from-user">
-                        <label htmlFor={"user"}>Select Type of user:</label>
-                        <Select name="user" id="user" value={this.state.userType} options={userTypes} 
-                            onChange={this.onTypeChange} components={animatedComponents}/>
+        if (!this.state.hasSuccess && !this.state.hasFail) {
+            return(
+                <div className = "Register_Ui">
+                    <div className = "Heading">
+                        <h1> Sign Up</h1>
+                        <br></br><br></br><br></br><br></br>
                     </div>
-                <input type="submit" className="login_Submit" id="navButton"/>
-            </div>
-
-        )
+                    <form onSubmit={this.onSubmit}>
+                        <div className = "UserName">
+                            <label htmlFor={"username"}>Username:</label>
+                            <input type="text" id="username" name="uname" required></input><br></br><br></br>
+                        </div>
+                        <div className = "Password">
+                            <label htmlFor={"password"}>Password:</label>
+                            <input type="password" id="password" name="pass" required></input><br></br><br></br>
+                        </div>
+                        <div className = "Phone">
+                            <label htmlFor={"phone"}>Phone:</label>
+                            <input type="number" id="phone" name="phone" required></input><br></br><br></br>
+                        </div>
+                        <div className = "Address">
+                            <label htmlFor={"address"}>Address:</label>
+                            <input type="text" id="address" name="address" required></input><br></br><br></br>
+                        </div>
+                        <div className = "Firstname">
+                            <label htmlFor={"firstName"}>First Name:</label>
+                            <input type="text" id="firstName" name="fName" required></input><br></br><br></br>
+                        </div>
+                        <div className = "Lastname">
+                            <label htmlFor={"lastName"}>Last Name:</label>
+                            <input type="text" id="lastName" name="lName" required></input><br></br><br></br>
+                        </div>
+                        <div className="form-user">
+                            <label htmlFor={"user"}>Select Type of user:</label>
+                            <Select name="user" id="user" value={this.state.userType} options={userTypes}
+                                    onChange={this.onTypeChange} components={animatedComponents} required/>
+                        </div>
+                        <input type="submit" className="login_Submit" id="navButton"/>
+                    </form>
+                </div>
+            )
+        }
+        if(this.state.hasSuccess) {
+            return(
+                <Router>
+                    <Route exact path="/">
+                        <Redirect to="/login" component = {LoginScreen}/>
+                    </Route>
+                </Router>
+            )
+        }
+        else if(this.state.hasFail){
+            alert("username is taken, please try again!")
+        }
     }
 }
 
