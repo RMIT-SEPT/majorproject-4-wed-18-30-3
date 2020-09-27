b161e671d95ec6a1cccc72e42ab6ea74c24b9674
s-brez <sam@sdbgroup.io>
2020-09-27 18:47:20 +1000

    add contextual props to availabilities screen

--- FrontEnd/frontend-react/src/components/AvailabilitiesScreen/AvailabilitiesScreen.js
+++ FrontEnd/frontend-react/src/components/AvailabilitiesScreen/AvailabilitiesScreen.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Header from '../Layout/Header';
+import Footer from '../Layout/Footer';
 
 class AvailabilitiesScreen extends Component {
     render() {
         return (
             <div>
-                <h1>Todo: Booking Screen</h1>
+                <Header
+                    id={this.props.id}
+                    userName={this.props.userName}
+                    address={this.props.address}
+                    phone={this.props.phone}
+                    userType={this.props.userType}
+                    token={this.props.token}/>
+                    <br/><br/><br/><br/>
+
+                <h1>View Availabilities</h1>
+
+                <Footer/>
             </div>
         )
     }
