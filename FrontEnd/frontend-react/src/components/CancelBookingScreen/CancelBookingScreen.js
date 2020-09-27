7de6d8ebfabc8f1d7d2bbc3e905547c949390e45
s-brez <sam@sdbgroup.io>
2020-09-27 19:20:46 +1000

    add CancellationScreen/pane skeleton

--- FrontEnd/frontend-react/src/components/CancelBookingScreen/CancelBookingScreen.js
+++ FrontEnd/frontend-react/src/components/CancelBookingScreen/CancelBookingScreen.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
-import Header from '../Layout/Header';
-import Footer from '../Layout/Footer';
+import NavPane from '../Layout/NavPane'
+import Header from '../Layout/Header'
+import Footer from '../Layout/Footer'
+import CancelBookingPane from './CancelBookingPane'
 
 class CancelBookingScreen extends Component {
     render() {
         return (
-            <div>
-                <Header
-                    id={this.props.id}
+            <div className="container" id="cancel_screen_container">
+                <Header id={this.props.id}
                     userName={this.props.userName}
                     address={this.props.address}
                     phone={this.props.phone}
                     userType={this.props.userType}
                     token={this.props.token}/>
-                    <br/><br/><br/><br/>
-
+                <br/><br/><br/>
+                
                 <h1>Cancel existing booking</h1>
 
+                <div className="row">
+                    <div className="col-sm-3">
+                        <NavPane
+                            id={this.props.id}
+                            userName={this.props.userName}
+                            address={this.props.address}
+                            phone={this.props.phone}
+                            userType={this.props.userType}
+                            token={this.props.token}/>
+                    </div>
+                    <div className="col-sm-9">
+                        <CancelBookingPane
+                            id={this.props.id}
+                            userName={this.props.userName}
+                            address={this.props.address}
+                            phone={this.props.phone}
+                            userType={this.props.userType}
+                            token={this.props.token}/>
+                    </div>
+                </div>
                 <Footer/>
             </div>
         )
