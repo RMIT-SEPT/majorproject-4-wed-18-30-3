30c202458f5d0795332988371b708408b7e95f59
s-brez <sam@sdbgroup.io>
2020-09-27 19:19:29 +1000

    add user contextual nav bar

--- FrontEnd/frontend-react/src/components/Layout/ContextualButtonPane.js
+++ FrontEnd/frontend-react/src/components/Layout/ContextualButtonPane.js
@@ -4,22 +4,70 @@ import {Link} from "react-router-dom";
 class ContextualButtonPane extends Component {
     
     render() {
-        return (
-            <React.Fragment>
-            <Link to="/dashboard" className="btn btn-sm btn-dark" id="navButton">
-            Todo: contextual links
-            </Link>
-            <br/><br/>
-            <Link to="/dashboard" className="btn btn-sm btn-dark" id="navButton">
-            Todo: contextual links
-            </Link>
-            <br/><br/>
-            <Link to="/dashboard" className="btn btn-sm btn-dark" id="navButton">
-            Todo: contextual links
-            </Link>
-            <br/><br/>
-            </React.Fragment>
-        )
+        
+        // Customer view
+        if (this.props.userType === "CUSTOMER") {
+            return (
+                <React.Fragment>              
+                                
+                <Link to="/bookings" className="btn btn-sm btn-dark" id="navButton">Create a new booking</Link>
+                <br/><br/>
+
+                <Link to="/cancel" className="btn btn-sm btn-dark" id="navButton">Cancel existing booking</Link>
+                <br/><br/>
+                
+                <Link to="/history" className="btn btn-sm btn-dark" id="navButton">View booking history</Link>
+                <br/><br/>
+
+                <Link to="/availabilites" className="btn btn-sm btn-dark" id="navButton">View availabilites</Link>
+                <br/><br/>
+
+                <Link to="/profile" className="btn btn-sm btn-dark" id="navButton">Edit profile</Link>
+                <br/><br/>
+
+                </React.Fragment>
+            )
+
+        // Admin nav view
+        } else if (this.props.userType === "ADMIN") {
+            return (
+                <React.Fragment>              
+                <Link to="/admin_portal" className="btn btn-sm btn-dark" id="navButton">View customer bookings</Link>
+                <br/><br/>
+
+                <Link to="/admin_portal" className="btn btn-sm btn-dark" id="navButton">Manage company groups</Link>
+                <br/><br/>
+
+                <Link to="/admin_portal" className="btn btn-sm btn-dark" id="navButton">Appointment settings</Link>
+                <br/><br/>
+
+                <Link to="/profile" className="btn btn-sm btn-dark" id="navButton">Edit profile</Link>
+                <br/><br/>
+                </React.Fragment>
+            )
+        
+        // Worker nav view
+        } else if (this.props.userType === "WORKER") {
+            return (
+                <React.Fragment>              
+                
+                <Link to="/set_availabilities" className="btn btn-sm btn-dark" id="navButton">Set availabilites</Link>
+                <br/><br/>
+
+                <Link to="/profile" className="btn btn-sm btn-dark" id="navButton">Edit profile</Link>
+                <br/><br/>
+                </React.Fragment>
+            )
+        
+        // No logged in user view
+        } else {
+            return (
+                <React.Fragment>              
+                <Link to="/login" className="btn btn-sm btn-dark" id="navButton">Please log in</Link>
+                <br/><br/>
+                </React.Fragment>
+            )
+        }
     }
 }
 
