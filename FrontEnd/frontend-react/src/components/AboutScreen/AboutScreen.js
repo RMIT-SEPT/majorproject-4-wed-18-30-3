71bc4ce8f42248ab89d33970a1b614881378ac48
s-brez <sam@sdbgroup.io>
2020-09-27 18:13:57 +1000

    add stateful routing and placeholder image to about screen

--- FrontEnd/frontend-react/src/components/AboutScreen/AboutScreen.js
+++ FrontEnd/frontend-react/src/components/AboutScreen/AboutScreen.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Header from '../Layout/Header';
+import Footer from '../Layout/Footer';
+import logo from './agme.png'
 
 class AboutScreen extends Component {
     render() {
         return (
             <div>
-                <h1>Todo: About screen</h1>
+                <Header
+                    id={this.props.id}
+                    userName={this.props.userName}
+                    address={this.props.address}
+                    phone={this.props.phone}
+                    userType={this.props.userType}
+                    token={this.props.token}/>
+                    <br/><br/><br/><br/>
+
+                <h1>AGME | Excellence in task scheduling</h1>
+
+                <div>
+                    <img src={logo}/>
+                </div>
+
+                <Footer/>
             </div>
         )
     }
