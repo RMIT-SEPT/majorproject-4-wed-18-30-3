2d0a90bb6c79c322a71ba32790d3738c2a8b2215
s-brez <sam@sdbgroup.io>
2020-09-30 16:22:40 +1000

    add nav pane to MyBookingsScreen

--- FrontEnd/frontend-react/src/components/MyBookingsScreen/MyBookingsScreen.js
+++ FrontEnd/frontend-react/src/components/MyBookingsScreen/MyBookingsScreen.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
-import Header from '../Layout/Header';
-import Footer from '../Layout/Footer';
+import NavPane from '../Layout/NavPane'
+import Header from '../Layout/Header'
+import Footer from '../Layout/Footer'
 
 class MyBookingsScreen extends Component {
     render() {
         return (
-            <div>
-                <Header
-                    id={this.props.id}
+            <div className="container" id="cancel_screen_container">
+                <Header id={this.props.id}
                     userName={this.props.userName}
                     address={this.props.address}
                     phone={this.props.phone}
                     userType={this.props.userType}
                     token={this.props.token}/>
-                <h1>My bookings</h1>
+                <br/><br/><br/>
+                
+                <h1>View current bookings</h1>
+
+                <div className="row">
+                    <div className="col-sm-3">
+                        <NavPane
+                            id={this.props.id}
+                            userName={this.props.userName}
+                            address={this.props.address}
+                            phone={this.props.phone}
+                            userType={this.props.userType}
+                            token={this.props.token}/>
+                    </div>
+                    <div className="col-sm-9">
+
+                    </div>
+                </div>
                 <Footer/>
             </div>
         )
