0d9fc3c52c5f92883b3fa0966ace90861e7dd4c5
s-brez <sam@sdbgroup.io>
2020-09-30 16:21:43 +1000

    add booking pane child component

---
+++ FrontEnd/frontend-react/src/components/MyBookingsScreen/MyBookingPane.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class MyBookingPane extends Component {
+    render() {
+        return (
+            <div className="history_pane" id="history_pane">
+                <br/>
+                <b>Upcoming bookings</b>
+                <p>TODO</p>
+                <br/><br/>
+            </div>
+        )
+    }
+}
+
+export default MyBookingPane;
