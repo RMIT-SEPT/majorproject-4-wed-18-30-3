5329cae4d28b3dc241123f2dad353751cfb85a7b
s-brez <sam@sdbgroup.io>
2020-09-30 14:37:28 +1000

    add book by timeslot functionality

--- FrontEnd/frontend-react/src/components/AvailabilitiesScreen/AvailabilitiesPane.js
+++ FrontEnd/frontend-react/src/components/AvailabilitiesScreen/AvailabilitiesPane.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom'
 import Select from 'react-select';
 import axios from "axios";
 import makeAnimated from 'react-select/animated';
+import CancelButton from './CancelButton';
 
 // How many timeslots to display at once in the drop down for timeslot view
-const SLOTS_TO_VIEW = 8
+const SLOTS_TO_VIEW = 7
 
 const DNS_URI = "http://localhost:8080"
 // const DNS_URI = "http://ec2-34-204-47-86.compute-1.amazonaws.com:8080"
@@ -26,6 +27,31 @@ function parseDateString(dStr) {
 const capitalise = (string) => {
     if (typeof string !== 'string') return null
     return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+// Header config for REST requests
+const axiosConfig = {headers: {'Content-Type': 'application/json'}}
+
+// Send a create booking request to bookings endpoint
+async function createBooking(newBooking) {
+
+    console.log(newBooking)
+    return await axios.post(DNS_URI + '/api/booking', {
+        updated_At: currentTime(),
+        worker: newBooking.worker,
+        timeslot: newBooking.timeslot,
+        service: newBooking.service,
+        customer: newBooking.customer
+    }, axiosConfig)
+    .then(res => {
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(res)
+        return true
+    })
+    .catch(error => {
+        console.error(error)
+        return false
+    })
 }
 
 function currentTime() {
@@ -56,28 +82,95 @@ class AvailabilitiesPane extends Component {
             avOptions: [],
             lastCurrentAvOption: null,
             selectedAv: null,
+            selectedTimeslot: null,
+            selectedWorker: null,
+            selectedService: null,
             viewMessage: null,
             viewMessageCount: null,
             viewIndex: SLOTS_TO_VIEW,
             bookingBtnMsg: "~",
-            textAreaMsg: ""
+            textAreaMsg: "",
+            userName: null,
+            userType: null,
+            token: null
         }
 
+        this.book = this.book.bind(this)
+        this.reset = this.reset.bind(this)
         this.loadNext = this.loadNext.bind(this)
-        this.loadPrev = this.loadPrev.bind(this)
         this.loadAvailabilityOptions = this.loadAvailabilityOptions.bind(this)
         this.onAvailabilityChange = this.onAvailabilityChange.bind(this)
         this.selectView = this.selectView.bind(this)
         this.timeslotView = this.timeslotView.bind(this)
         this.workerView = this.workerView.bind(this)
+        this.storeLoginToken = this.storeLoginToken.bind(this)   
     }
 
-    timeslotView () {this.setState({view: "timeslot"}, this.setState({availabilites: this.loadAvailabilityOptions()}))}
-    selectView () {this.setState({view: "select"})}
-    workerView () {this.setState({view: "worker"})}
+    async selectView () {this.setState({view: "select"})}
+    
+    async timeslotView () {
+        this.storeLoginToken()
+        this.setState({availabilites: this.loadAvailabilityOptions()})
+        this.setState({view: "timeslot"})
+    }
+    
+    async workerView () {
+        this.storeLoginToken()
+        this.setState({view: "worker"})
+    }
 
-    onChange(e){
+    async storeLoginToken() {
+        const name = document.getElementById("userName").textContent
+        const type = document.getElementById("userType").textContent
+        const loginToken = document.getElementById("userToken").textContent
+        
+        this.setState({userName: name})
+        this.setState({userType: type})
+        this.setState({token: loginToken})
+    }
+    
+    async onChange(e){
+        e.preventDefault()
+    }
+    
+    async book () {
+        
+        const name = document.getElementById("userName").textContent
+        const type = document.getElementById("userType").textContent
+        const loginToken = document.getElementById("userToken").textContent
+        const selectedService = document.getElementById("selectedService").textContent
+        const selectedWorker = document.getElementById("selectedWorker").textContent
+        const selectedTimeslot = document.getElementById("selectedTimeslot").textContent
 
+        // Validate login
+        if (name !== null && type !== null) {
+            // Validate user type
+            if(type !== "CUSTOMER") {
+                alert("You must be a customer to make bookings. Create a customer account and try again.")
+                return 
+            }
+        } else {
+            alert("You must be logged in to make bookings. Please log in.")
+            return 
+        }
+
+        // Send the POST request
+        const success = await createBooking({
+            updated_At: currentTime(),
+            worker: {user: {userName: selectedWorker}},
+            timeslot: {date: selectedTimeslot},
+            service: {name: selectedService},
+            customer: {user: {userName: name}}
+        }).then()
+
+        console.log(success)
+
+        // Set success/fail state, will change what the pane is rendering
+        if (success) {
+            this.setState({view: "success"})
+        } else {
+            this.setState({view: "failure"})
+        }
     }
 
     // Store availabilities in component state
@@ -111,8 +204,6 @@ class AvailabilitiesPane extends Component {
         var count = 0
         const avs = this.state.availabilites
         var avOptions = []
-        
-        
 
         // Only add timeslots to options if they are in the future
         for (let i = 0; i < avs.length; i++) {   
@@ -120,21 +211,23 @@ class AvailabilitiesPane extends Component {
             if (date > start && count < SLOTS_TO_VIEW && count < avs.length) {
                 count++
                 if (avs[i + step] !== null) {
-                    avOptions.push({
+                    const av = {
                         value: {
                             date: avs[i + step]["timeslot"]["date"], 
                             worker: avs[i + step]["worker"]["user"]["userName"], 
                             service: avs[i + step]["worker"]["services"][0]["name"]}, 
-                        label: parseDateString(avs[i + step]["timeslot"]["date"]).toUTCString()})
+                        label: parseDateString(avs[i + step]["timeslot"]["date"]).toUTCString()}
+                    avOptions.push(av)
+
+                    // select the first available appointment by default
+                    if (step === 1) {
+
+                    }
                 }
                 step = 0
             }
         }
         this.setState({avOptions: avOptions})
-
-        console.log(avOptions)
-
-        console.log(avOptions[avOptions.length - 1])
 
         this.setState({lastCurrentAvOption: avOptions[avOptions.length - 1]["value"]["date"]})
         var msg = "(displaying " + (this.state.viewIndex - SLOTS_TO_VIEW)  + "-" + this.state.viewIndex + " of " + this.state.availabilites.length + ")"
@@ -152,20 +245,30 @@ class AvailabilitiesPane extends Component {
 
     // Load the availability data in text area and enable booking button
     async onAvailabilityChange(av) {
-        this.setState({selectedAv: av})
     
         var avMsg = "You have selected " + av["value"]["service"] + " with " +
                     av["value"]["worker"] + ", " + av["label"] + "."
+                    
+        var service = av["value"]["service"]
 
+        var worker = av["value"]["worker"]
+        
+        var timeslot = av["value"]["date"]
+
+        this.setState({selectedAv: av})
         this.setState({bookingBtnMsg: "Book selected timeslot"})
         this.setState({selectedAvMsg: avMsg})        
+        this.setState({selectedService: service})
+        this.setState({selectedWorker: worker})
+        this.setState({selectedTimeslot: timeslot})
+        
     }
 
     // Load next batch of av's
     async loadNext() {
         
         var avs = await this.getAvailabilities()
-        if (this.state.viewIndex != avs.length) {
+        if (this.state.viewIndex !== avs.length) {
             
             // Adjust display message
             if (this.state.viewIndex + SLOTS_TO_VIEW > avs.length) {
@@ -176,21 +279,50 @@ class AvailabilitiesPane extends Component {
 
             // Load next batch of slots
             this.loadAvailabilityOptions(this.state.lastCurrentAvOption)
-        }
 
+            this.setState({bookingBtnMsg: "~"})
+            this.setState({selectedAvMsg: ""})        
+            this.setState({selectedService: ""})
+            this.setState({selectedWorker: ""})
+            this.setState({selectedTimeslot: ""})
+        }
     }
 
-    // Load last batch of av's
-    async loadPrev() {
-
+    reset () {
+        this.setState({view: "select"})
+        this.setState({availabilites: this.getAvailabilities()})
+        this.setState({avOptions: []})
+        this.setState({lastCurrentAvOption: null})
+        this.setState({selectedAv: null})
+        this.setState({viewMessage: null})
+        this.setState({viewMessageCount: null})
+        this.setState({viewIndex: SLOTS_TO_VIEW})
+        this.setState({bookingBtnMsg: "~"})
+        this.setState({textAreaMsg: ""})
+        this.setState({selectedAvMsg: ""})
     }
 
     render() {
         
         const animatedComponents = makeAnimated();
 
+        const id = this.props.id
+        const userName = this.props.userName
+        const userType = this.props.userType
+        const token = this.props.token
+
+        // Check user is logged in
+        if (this.props.userName === undefined && this.props.userType === undefined) {
+            return (
+                <div className="cancel_pane" id="cancel_pane">
+                    <br/>
+                    <b>Please log in to view availabilites</b>
+                    <br/><br/>
+                </div>
+            )
+
         // Default view, user needs to make a selection
-        if (this.state.view === "select") {
+        } else if (this.state.view === "select") {
             return (
                 <div className="cancel_pane" id="cancel_pane">
                     <br/>
@@ -208,14 +340,48 @@ class AvailabilitiesPane extends Component {
                             </button> 
                         </div>
                     </div>
+                    <div id="userId" style={{display: "none"}}>{id}</div>
+                    <div id="userName" style={{display: "none"}}>{userName}</div>
+                    <div id="userType" style={{display: "none"}}>{userType}</div>
+                    <div id="userToken" style={{display: "none"}}>{token}</div>
                 </div>
             )
         
+         // Worker view
+        } else if (this.state.view === "worker") {
+            return (
+                <div className="timeslot_view_pane" id="timeslot_view_pane">
+                    <br/>
+                    <b>Availabilites by worker</b>
+                 
+
+                    <div className="row">
+                        <div className="col-sm">
+                            Select a worker to view availabilities
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-sm">
+                            <button className="btn btn-sm btn-dark" id="navButton" onClick={this.selectView}>
+                                Back to select view
+                            </button> 
+                        </div>
+                        <div className="col-sm">
+                            <Link to="/Dashboard" className="btn btn-sm btn-dark" id="navButton">Cancel</Link>    
+                        </div>
+                    </div>
+                    <div id="userId" style={{display: "none"}}>{id}</div>
+                    <div id="userName" style={{display: "none"}}>{userName}</div>
+                    <div id="userType" style={{display: "none"}}>{userType}</div>
+                    <div id="userToken" style={{display: "none"}}>{token}</div>
+                </div>
+            )
+
         // Timeslot view
         } else if (this.state.view === "timeslot") {
 
             const next = "next " + SLOTS_TO_VIEW + " slots >>"
-            const prev = "<< prev " + SLOTS_TO_VIEW + " slots"
             
             return (
                 <div className="timeslot_view_pane" id="timeslot_view_pane">
@@ -230,7 +396,9 @@ class AvailabilitiesPane extends Component {
 
                     <div className="row">
                         <div className="col-2">
-                            <button className="btn btn-sm btn-dark" id="navButton" onClick={this.loadPrev}>{prev}</button> 
+                            <Link to="/availabilites">
+                                <div className="btn btn-sm btn-dark" id="navButton" onClick={this.reset}>Reset</div> 
+                            </Link>
                         </div>
                         <div className="col-8">
                             <Select name={"availabilities"} components={animatedComponents}
@@ -256,6 +424,9 @@ class AvailabilitiesPane extends Component {
 
 
                     <b><font color="MidnightBlue">{this.state.selectedAvMsg}</font></b>
+                        <div id="selectedService" style={{display: "none"}}>{this.state.selectedService}</div>
+                        <div id="selectedWorker" style={{display: "none"}}>{this.state.selectedWorker}</div>
+                        <div id="selectedTimeslot" style={{display: "none"}}>{this.state.selectedTimeslot}</div>
                     <br/><br/>
 
                     <div className="row">
@@ -277,36 +448,57 @@ class AvailabilitiesPane extends Component {
                             <Link to="/Dashboard" className="btn btn-sm btn-dark" id="navButton">Cancel</Link>    
                         </div>
                     </div>
+                    <div id="userId" style={{display: "none"}}>{id}</div>
+                    <div id="userName" style={{display: "none"}}>{userName}</div>
+                    <div id="userType" style={{display: "none"}}>{userType}</div>
+                    <div id="userToken" style={{display: "none"}}>{token}</div>
                 </div>
             )
              
-        // Worker view
-        } else if (this.state.view === "worker") {
+        // Successful booking
+        } else if (this.state.view === "success") {
             return (
-                <div className="timeslot_view_pane" id="timeslot_view_pane">
-                    <b>Availabilites by worker</b>
-                 
-
+                <div className="booking_screen_bookingpane" id="booking_screen_bookingpane">
+                    <br/>    
+                    <b>Booking placed successfully. Thanks, {this.props.userName}.</b>
+                    <br/><br/>   
+                    <Link to="/my_bookings">View your bookings.</Link>
+                    <br/><br/>  
                     <div className="row">
                         <div className="col-sm">
-                            Select a worker to view availabilities
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-sm">
-                            <button className="btn btn-sm btn-dark" id="navButton" onClick={this.selectView}>
-                                Back to select view
+                            <button className="btn btn-sm btn-dark" id="navButton" onClick={this.reset}>
+                                Make another booking
                             </button> 
                         </div>
                         <div className="col-sm">
-                            <Link to="/Dashboard" className="btn btn-sm btn-dark" id="navButton">Cancel</Link>    
+                            <CancelButton/>
                         </div>
                     </div>
+                </div>   
+            )
+
+        // Failed booking
+        } else if (this.state.view === "failure") {
+            return (
+                <div className="booking_screen_bookingpane" id="booking_screen_bookingpane">
+                    <br/>    
+                    <b>Booking failed. Please try again.</b>
+                    <br/><br/>   
+                
+                <div className="row">
+                    <div className="col-sm">
+                        <button className="btn btn-sm btn-dark" id="navButton" onClick={this.reset}>
+                            Make another booking
+                        </button> 
+                    </div>
+                    <div className="col-sm">
+                        <CancelButton/>
+                    </div>
                 </div>
+            </div>  
             )
         }
     }
 }
 
-export default AvailabilitiesPane;
+export default AvailabilitiesPane
