e0abd088a4de6be497547cc1da8fe41a749d5877
Cameron vanRoon s3382032 <s3382032@student.rmit.edu.au>
2020-10-07 21:06:46 +1100

    Added getBookings() to worker

--- BackEnd/MajorProject/src/main/java/labwed18303/demo/model/Worker.java
+++ BackEnd/MajorProject/src/main/java/labwed18303/demo/model/Worker.java
@@ -79,6 +79,14 @@ public class Worker {
         return id;
     }
 
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
     public Set<ServiceProvided> getServices() {
         return services;
     }
