9dc43c3345c8f47cb7ced44755081e0bdf883303
Cameron vanRoon s3382032 <s3382032@student.rmit.edu.au>
2020-10-07 21:37:45 +1100

    Set Token duration 100 minutes

--- BackEnd/MajorProject/src/main/java/labwed18303/demo/security/SecurityConstants.java
+++ BackEnd/MajorProject/src/main/java/labwed18303/demo/security/SecurityConstants.java
@@ -10,6 +10,6 @@ public class SecurityConstants {
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000; //300 seconds - 5 minutes
+    public static final long EXPIRATION_TIME = 6000_000; //300 seconds - 5 minutes
 
 }
