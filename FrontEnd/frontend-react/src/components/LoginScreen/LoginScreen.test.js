0d098f204117922514823f564206cbbc7b747e60
s3719826 <41975799+s3719826@users.noreply.github.com>
2020-10-17 03:15:34 +1100

    Render tests now working, still need to work on the functioning tests

--- FrontEnd/frontend-react/src/components/LoginScreen/LoginScreen.test.js
+++ FrontEnd/frontend-react/src/components/LoginScreen/LoginScreen.test.js
@@ -3,9 +3,25 @@ import { configure, shallow } from 'enzyme';
 import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16.3'
 import LoginScreen from './LoginScreen';
+import RegisterScreen from "../RegisterScreen/RegisterScreen";
 
 configure({ adapter: new Adapter() });
-
+//FUNCTIONAL TESTS
+// describe('<RegisterScreen/>', function() {
+//     it('tests if creating user has succeeded', function() {
+//         const wrapper = shallow(<RegisterScreen/>);
+//         const instance = wrapper.instance();
+//         instance.setState({userName:"abcd"})
+//         instance.setState({password:"pass"})
+//         instance.setState({phone:"0000000000"})
+//         instance.setState({address:"123"})
+//         instance.setState({firstName:"abcd"})
+//         instance.setState({lastName:"efgh"})
+//         instance.setState({userType:{value:'CUSTOMER', label: 'Customer'}})
+//         expect(wrapper.state.response).to.equal("200");
+//     });
+// });
+//RENDER TESTS
 describe('<LoginScreen/>', function() {
     it('renders heading text', function() {
         const wrapper = shallow(
@@ -14,6 +30,33 @@ describe('<LoginScreen/>', function() {
             </LoginScreen>
         );
         expect(wrapper.contains(<h1>Log in</h1>)).to.equal(true);
+    });
+});
+describe('<LoginScreen/>', function() {
+    it('renders username', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="userName"/>));
+    });
+});
+describe('<LoginScreen/>', function() {
+    it('renders password', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="password"/>));
+    });
+});
+describe('<LoginScreen/>', function() {
+    it('renders button submit', function() {
+        const wrapper = shallow(
+            <LoginScreen>
+                <input type="submit" value="Sign in" className="btn btn-outline-dark" id="navButton"/>
+            </LoginScreen>
+        );
+        expect(wrapper.contains(<input type="submit" value="Sign in" className="btn btn-outline-dark" id="navButton"/>)).to.equal(true);
+    })});
+describe('<LoginScreen/>', function() {
+    it('renders button register', function() {
+        const wrapper = shallow(<LoginScreen/>);
+        expect(wrapper.find(<a href="/register" className="btn btn-outline-dark" id="navButton"/>));
     });
 });
 // describe('<LoginScreen/>', function() {
--- FrontEnd/frontend-react/src/components/RegisterScreen/RegisterScreen.test.js
+++ FrontEnd/frontend-react/src/components/RegisterScreen/RegisterScreen.test.js
@@ -6,7 +6,25 @@ import RegisterScreen from './RegisterScreen';
 import Select from 'react-select';
 
 configure({ adapter: new Adapter() });
+//FUNCTIONAL TESTS
 
+// describe('<RegisterScreen/>', function() {
+//     it('tests if creating user has succeeded', function() {
+//         const wrapper = shallow(<RegisterScreen/>);
+//         const instance = wrapper.instance();
+//         instance.setState({userName:"abcd"})
+//         instance.setState({password:"pass"})
+//         instance.setState({phone:"0000000000"})
+//         instance.setState({address:"123"})
+//         instance.setState({firstName:"abcd"})
+//         instance.setState({lastName:"efgh"})
+//         instance.setState({userType:{value:'CUSTOMER', label: 'Customer'}})
+//         expect(wrapper.state.response).to.equal("200");
+//     });
+// });
+
+
+// RENDER TESTS
 describe('<RegisterScreen/>', function() {
     it('renders heading text', function() {
         const wrapper = shallow(
@@ -17,150 +35,59 @@ describe('<RegisterScreen/>', function() {
         expect(wrapper.contains(<h1>Register</h1>)).to.equal(true);
     });
 });
-// describe('<RegisterScreen/>', function() {
-//     it('renders username label', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <label htmlFor={"username"}>Username:</label>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<label htmlFor={"username"}>Username:</label>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders username input', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <input type="text" id="username" name="uname" required></input>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<input type="text" id="username" name="uname" required></input>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders password label', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <label htmlFor={"password"}>Password:</label>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<label htmlFor={"password"}>Password:</label>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders password input', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <input type="password" id="password" name="pass" required></input>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<input type="password" id="password" name="pass" required></input>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders phone label', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <label htmlFor={"phone"}>Phone:</label>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<label htmlFor={"phone"}>Phone:</label>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders phone input', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <input type="number" id="phone" name="phone" required></input>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<input type="number" id="phone" name="phone" required></input>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders address label', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <label htmlFor={"address"}>Phone:</label>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<label htmlFor={"address"}>Address:</label>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders address input', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <input type="text" id="address" name="address" required></input>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<input type="text" id="address" name="address" required></input>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders first name label', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <label htmlFor={"firstName"}>Phone:</label>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<label htmlFor={"firstName"}>First Name:</label>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders first name input', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <input type="text" id="firstName" name="fName" required></input>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<input type="text" id="firstName" name="fName" required></input>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders last name label', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <label htmlFor={"lastName"}>Phone:</label>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<label htmlFor={"lastName"}>Last Name:</label>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders last name input', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <input type="text" id="lastName" name="lName" required></input>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<input type="text" id="lastName" name="lName" required></input>)).to.equal(true);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders one <Select/> component', function() {
-//         const wrapper = shallow(<RegisterScreen/>);
-//         expect(wrapper.find(Select)).to.have.lengthOf(1);
-//     });
-// });
-// describe('<RegisterScreen/>', function() {
-//     it('renders user type label', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <label htmlFor={"user"}>Select Type of user:</label>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<label htmlFor={"user"}>Select Type of user:</label>)).to.equal(true);
-//     });
-// });
-
-// describe('<RegisterScreen/>', function() {
-//     it('renders button submit', function() {
-//         const wrapper = shallow(
-//             <RegisterScreen>
-//                 <input type="submit" className="register_Submit" id="navButton"/>
-//             </RegisterScreen>
-//         );
-//         expect(wrapper.contains(<input type="submit" className="register_Submit" id="navButton"/>)).to.equal(true);
-//     });
-// });
+describe('<RegisterScreen/>', function() {
+    it('renders username', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="userName"/>));
+    });
+});
+describe('<RegisterScreen/>', function() {
+    it('renders password', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="password"/>));
+    });
+});
+describe('<RegisterScreen/>', function() {
+    it('renders phone', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="phone"/>));
+    });
+});
+describe('<RegisterScreen/>', function() {
+    it('renders address', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="address"/>));
+    });
+});
+describe('<RegisterScreen/>', function() {
+    it('renders first name', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="firstName"/>));
+    });
+});
+describe('<RegisterScreen/>', function() {
+    it('renders last name', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<input type="text" id="lastName"/>));
+    });
+});
+describe('<RegisterScreen/>', function() {
+    it('renders one <Select/> component', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(Select)).to.have.lengthOf(1);
+})});
+describe('<RegisterScreen/>', function() {
+    it('renders button submit', function() {
+        const wrapper = shallow(
+            <RegisterScreen>
+                <input type="submit" className="btn btn-outline-dark" id="navButton"/>
+            </RegisterScreen>
+        );
+        expect(wrapper.contains(<input type="submit" className="btn btn-outline-dark" id="navButton"/>)).to.equal(true);
+})});
+describe('<RegisterScreen/>', function() {
+    it('renders button log in', function() {
+        const wrapper = shallow(<RegisterScreen/>);
+        expect(wrapper.find(<a href="/login" className="btn btn-outline-dark" id="navButton"/>));
+    });
+});
